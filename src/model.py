7ab908b67115c854ca1552349c799ca0398ce216
sadulla68 <sadullathirudi@gmail.com>
2026-02-17 23:14:46 +0530

    Implement LSTM model building function

---
+++ src/model.py
@@ -0,0 +1,13 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+
+def build_model(input_shape):
+    model = Sequential()
+    model.add(LSTM(64, return_sequences=True, input_shape=input_shape))
+    model.add(Dropout(0.2))
+    model.add(LSTM(64))
+    model.add(Dropout(0.2))
+    model.add(Dense(1))
+
+    model.compile(optimizer="adam", loss="mse")
+    return model
